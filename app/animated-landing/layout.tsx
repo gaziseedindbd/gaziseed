4c1bbbe372d656b42e8e1cedd1cf8a7915a3f4dc
superkingseed <superkingseedbd@gmail.com>
2026-09-02 22:24:11 +0530

    Force animated order confirmation overlay globally

--- app/animated-landing/layout.tsx
+++ app/animated-landing/layout.tsx
@@ -1,4 +1,5 @@
 import './animated-shell.css';
+import './order-success-force.css';
 import styles from './animated-pro-safe.module.css';
 import successStyles from './order-success.module.css';
 import successFixStyles from './order-success-fix-v2.module.css';
