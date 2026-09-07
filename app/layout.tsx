92eee3b4df47c2fba4ec49eb7978692fdcf32831
gaziseedindbd <gaziseedbd@gmail.com>
2026-09-07 07:38:53 +0530

    Load final Design 2 category card styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="stylesheet" href="/home-category-master-v10.css?v=10" />
         <link rel="stylesheet" href="/home-category-atelier-v12.css?v=12" />
         <link rel="stylesheet" href="/home-category-atelier-v13.css?v=13" />
+        <link rel="stylesheet" href="/home-category-design2-final.css?v=2" />
         <script dangerouslySetInnerHTML={{ __html: `(function(){window.__GAZI_COUNTRY__='${visitorCountry}';})();` }} />
         <script dangerouslySetInnerHTML={{ __html: `(function(){try{const theme=localStorage.getItem('admin_theme');if(theme==='dark'){document.documentElement.classList.add('dark')}else{document.documentElement.classList.remove('dark')}}catch(e){}})()` }} />
         <script dangerouslySetInnerHTML={{ __html: `(function(){if('serviceWorker' in navigator){window.addEventListener('load',function(){navigator.serviceWorker.register('/sw.js').catch(function(){})}})()` }} />
