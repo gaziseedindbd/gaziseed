36ea4d971762d11b562929bd660ed3b44f49fdb1
gaziseedindbd <gaziseedbd@gmail.com>
2026-09-23 10:50:05 +0530

    fix: use absolute sitemap URL in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 
+const SITE_URL = (process.env.NEXT_PUBLIC_SITE_URL || 'https://www.gaziseed.com').replace(/\/$/, '');
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
@@ -9,6 +11,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/admin', '/admin/*', '/account', '/cart', '/checkout', '/order-success', '/login', '/register', '/force-password-change'],
       },
     ],
-    sitemap: '/sitemap.xml',
+    sitemap: SITE_URL + '/sitemap.xml',
   };
 }
