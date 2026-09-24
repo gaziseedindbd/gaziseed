a47118a7eb7a3cf3aa96856ad03e5d72d51195f0
gaziseedindbd <gaziseedbd@gmail.com>
2026-09-24 10:08:59 +0530

    Remove legacy order branch header from orders layout

--- app/admin/orders/layout.tsx
+++ app/admin/orders/layout.tsx
@@ -1,10 +1,3 @@
-import { OrderBranchHeader } from '@/components/admin/order-branch-header';
-
 export default function OrdersLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <OrderBranchHeader />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
